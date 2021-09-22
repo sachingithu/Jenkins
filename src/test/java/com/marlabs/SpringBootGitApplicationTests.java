e10584d751cd1d514df7ce65f716997b825dcf03
Sachin <jadhavsachin5858@gmail.com>
2021-09-22 10:38:45 +0530

    Second Commit

--- src/test/java/com/marlabs/SpringBootGitApplicationTests.java
+++ src/test/java/com/marlabs/SpringBootGitApplicationTests.java
@@ -1,13 +1,22 @@
 package com.marlabs;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootGitApplicationTests {
-
+	public static Logger logger=LoggerFactory.getLogger(SpringBootGitApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Text Case Executing.....");
+		logger.info("Text Case Executing Second Log Statement.....");
+		assertEquals(true, true);
 	}
 
 }
